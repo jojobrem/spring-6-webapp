4cabdbd28fc8909ba3278a4c47377890b79147e2
jojobrem <jainsbury@yahoo.com>
2025-07-20 15:55:42 +0100

    init commit

--- src/main/java/guru/springframework/spring6webapp/Spring6WebappApplication.java
+++ src/main/java/guru/springframework/spring6webapp/Spring6WebappApplication.java
@@ -3,11 +3,13 @@ package guru.springframework.spring6webapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import static org.springframework.boot.SpringApplication.*;
+
 @SpringBootApplication
 public class Spring6WebappApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Spring6WebappApplication.class, args);
+        run(Spring6WebappApplication.class, args);
     }
 
 }
