082cf498c22b42d40448cef35edb41dadc3cf1b2
jojobrem <jainsbury@yahoo.com>
2025-07-20 17:18:00 +0100

    compare

--- src/main/java/guru/springframework/spring6webapp/Spring6WebappApplication.java
+++ src/main/java/guru/springframework/spring6webapp/Spring6WebappApplication.java
@@ -3,13 +3,11 @@ package guru.springframework.spring6webapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import static org.springframework.boot.SpringApplication.*;
-
 @SpringBootApplication
 public class Spring6WebappApplication {
 
     public static void main(String[] args) {
-        run(Spring6WebappApplication.class, args);
+        SpringApplication.run(Spring6WebappApplication.class, args);
     }
 
 }
---
+++ src/main/java/guru/springframework/spring6webapp/bootstrap/BootstrapData.java
@@ -0,0 +1,50 @@
+package guru.springframework.spring6webapp.bootstrap;
+
+import guru.springframework.spring6webapp.domain.Author;
+import guru.springframework.spring6webapp.domain.Book;
+import guru.springframework.spring6webapp.repositories.AuthorRepository;
+import guru.springframework.spring6webapp.repositories.BookRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+@Component
+public class BootstrapData implements CommandLineRunner {
+
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+
+    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Author eric = new Author();
+        eric.setFirstName("Eric");
+        eric.setLastName("Evans");
+
+        Book ddd = new Book();
+        ddd.setTitle("Domain Driven Design");
+        ddd.setIsbn("123L");
+
+        Author ericSaved = authorRepository.save(eric);
+        Book dddSaved = bookRepository.save(ddd);
+
+        Author enid = new Author();
+        enid.setFirstName("Enid");
+        enid.setLastName("Blyton");
+
+        Book faraway = new Book();
+        faraway.setTitle("The Magic Faraway Tree");
+        faraway.setIsbn("345");
+
+        Author enidSaved = authorRepository.save(enid);
+        Book farawaySaved = bookRepository.save(faraway);
+
+        System.out.println("In Bootstrap");
+        System.out.println("Author Count: " + authorRepository.count());
+        System.out.println("In Bootstrap" + bookRepository.count());
+
+
+    }
+}
